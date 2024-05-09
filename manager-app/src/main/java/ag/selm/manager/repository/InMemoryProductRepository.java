4f3849fc52a7fc4394b260c789fe8888d3b20109
denis <denis.the.student@gmail.com>
2024-05-09 21:45:54 +0300

    lesson 1

---
+++ manager-app/src/main/java/ag/selm/manager/repository/InMemoryProductRepository.java
@@ -0,0 +1,46 @@
+package ag.selm.manager.repository;
+
+import ag.selm.manager.entity.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+@Repository
+public class InMemoryProductRepository implements ProductRepository {
+
+    private final List<Product> products = Collections.synchronizedList(new LinkedList<>());
+
+    public InMemoryProductRepository() {
+        IntStream.range(1, 4)
+                .forEach(i -> this.products.add(new Product(i, "Продукт №%d".formatted(i),
+                        "Описание продукта №%d".formatted(i))));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return Collections.unmodifiableList(this.products);
+    }
+
+    @Override
+    public Product save(Product product) {
+        product.setId(products.stream()
+                .max(Comparator.comparingInt(Product::getId))
+                .map(Product::getId)
+                .orElse(0) + 1);
+        this.products.add(product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(Integer productId) {
+        return this.products.stream()
+                .filter(product -> Objects.equals(productId, product.getId()))
+                .findFirst();
+    }
+
+    @Override
+    public void deleteById(Integer productId) {
+        this.products.removeIf(product -> Objects.equals(productId, product.getId()));
+    }
+}
---
+++ manager-app/src/main/java/ag/selm/manager/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package ag.selm.manager.repository;
+
+import ag.selm.manager.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+    List<Product> findAll();
+
+    Product save(Product product);
+
+    Optional<Product> findById(Integer productId);
+
+    void deleteById(Integer productId);
+}
